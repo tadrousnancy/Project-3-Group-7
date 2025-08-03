9c5839365fd2b80a7a5dd168c8f3dfb2d0b3fafb
HuyMama <148918916+HuyMama@users.noreply.github.com>
2025-08-03 14:17:36 -0400

    Update main.py
    Final Formatting Changes

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ from PrepareData import load_data, plot_trades
 
 def main():
 
+    print("\n==============================================================================\n")
     print("Welcome to TradingBot Simulator!\n")
     print("You are able to choose any US stock or ETF and the TradingBot will buy or sell that stock/ETF using the historical market dataset (From 1983 to 2019).\n")
-    print("The bot will complete the simulation process 3 times using 3 different algorithms, and output the final profits and total trades it has made: ")
+    print("The bot will complete the simulation process 3 times using 3 different algorithms:")
     print(" - Moving Average Crossover Strategy")
     print(" - RSI Strategy Trades")
-    print(" - Bollinger Bands Strategy Trades\n")
+    print(" - Bollinger Bands Strategy Trades")
+    print("and output the final profits and total trades it has made.\n") 
     print("The TradingBot will then determine which algorithm generates the most profit and then visualize the buying/selling data of each algorithm.\n")
-    
+    print("==============================================================================\n")
+
     while True:
         
         # Asks for input of Stock/ETF ticker symbol to test
         print("Here is a list of all US stocks: https://www.tradingview.com/markets/stocks-usa/market-movers-all-stocks/")
         print ("Here is a list of all US ETFs: https://www.tradingview.com/etf-screener/\n")
-        ticker_symbol = input("Enter the ticker symbol of any US stock or ETF you want to test (e.g. aapl for Apple). To exit, type 'exit': \n").strip().lower()
+        ticker_symbol = input("Enter the ticker symbol of any US stock or ETF you want to test (e.g. aapl or AAPL for Apple). To exit, type 'exit': \n").strip().lower()
 
         # Exits program if user types 'exit'
         if ticker_symbol == "exit":
@@ -30,23 +33,23 @@ def main():
         if not stock_to_test.endswith('.us'):
             stock_to_test += '.us'
 
-        print(f"\nChecking if '{ticker_symbol}' is a valid stock/ETF.\n")
+        print(f"\nChecking if '{ticker_symbol.upper()}' is a valid stock/ETF.\n")
 
         # Load prices for the selected stock/ETF
         prices, stock_df = load_data(stock_to_test)
 
         # Checks if stock/ETF is valid
         if stock_df.empty:
-            print(f"No data found for ticker '{ticker_symbol}'. Please try again.\n")
-            print("=========================================================================\n")
+            print(f"No data found for ticker '{ticker_symbol.upper()}'. Please try again.\n")
+            print("==============================================================================\n")
             continue
 
-        print(f"Stock/ETF found! You selected: {ticker_symbol}\n")
+        print(f"Stock/ETF found! You selected: '{ticker_symbol.upper()}'\n")
         
         # Asks user to input a starting balance (must be positive)
         while True:
             try:
-                starting_balance = float(input("Enter your starting balance (must be a positive number): "))
+                starting_balance = float(input("Enter the bot's initial balance (must be a positive number): "))
                 if starting_balance > 0:
                     break
                 else:
@@ -54,10 +57,10 @@ def main():
             except ValueError:
                 print("Invalid input. Please enter a number.")
         
-        print(f"\nStarting Balance: ${starting_balance}\n")
+        print("\n==============================================================================\n")
 
         # Runs the TradingBot Simulator
-        print("\nExecuting TradingBot Simulator...\n")
+        print(f"Executing TradingBot Simulator for '{ticker_symbol.upper()}' with an Initial Balance of ${starting_balance}:")
         
         # Create separate bots for each algorithms
         bot_ma = TradingBot(starting_balance)
@@ -70,7 +73,7 @@ def main():
         bot_bb.bollinger_bands_strategy(prices)
         
         # Print summary for each bot
-        print("\n===========Results===========\n")
+        print("\n===================================RESULTS====================================\n")
         print("Moving Average Crossover Strategy")
         print(f"Final Balance: ${bot_ma.current_balance:.2f}")
         print(f"Total Trades: {bot_ma.total_trades}\n")
@@ -83,7 +86,7 @@ def main():
         print(f"Final Balance: ${bot_bb.current_balance:.2f}")
         print(f"Total Trades: {bot_bb.total_trades}\n")
 
-        print("=============================\n")
+        print("==============================================================================\n")
         
         # Calculates which algorithm has the highest ending balance
         balances = {
@@ -93,11 +96,11 @@ def main():
         }
 
         best_algorithm = max(balances, key=balances.get)
-        print(f"\nBest Strategy: {best_algorithm} with a final balance of ${balances[best_algorithm]:.2f}")
+        print(f"Best Strategy: {best_algorithm} with a final balance of ${balances[best_algorithm]:.2f}")
 
         # Plots the trades that each algorithm made
         print("\nThe visualization of the data will be shown: Exit the plot to continue.\n")
-        print("=========================================================================\n")
+        print("==============================================================================\n")
         plot_trades(stock_df, bot_ma, bot_rsi, bot_bb, stock_to_test, starting_balance)
 
 
